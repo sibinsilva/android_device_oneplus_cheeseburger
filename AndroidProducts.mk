d8398bc9e31e7d954205cd41c4964139679260c2
Maitreya Patni <56203318+Maitreya29@users.noreply.github.com>
2020-12-30 19:44:03 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cheeseburger.mk
+    $(LOCAL_DIR)/pex_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cheeseburger-user \
-    aosp_cheeseburger-userdebug \
-    aosp_cheeseburger-eng
+    pex_cheeseburger-user \
+    pex_cheeseburger-userdebug \
+    pex_cheeseburger-eng
